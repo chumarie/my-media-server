c17eff73e9765115d0fc0f07a04124a1e34cfdf6
Chu Marie <marie.chu@tekos.net>
2023-04-09 17:33:25 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <h1>Marie</h1>
+        <button onClick={() => setCount((count) => count + 6)}>
           count is {count}
         </button>
         <p>
