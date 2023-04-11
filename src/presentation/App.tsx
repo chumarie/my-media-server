4a4abfe9b8d88cfe3e5e6b4799f6e98eb3640dee
Chu Marie <marie.chu@tekos.net>
2023-04-11 21:25:41 +0700

    chore - init tailwindcss

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,6 +1,7 @@
 import AppRoutes from '@presentation/routing/AppRoutes';
 
 import './styles/global.css';
+import './styles/tailwind.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
