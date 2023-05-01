38d6bb0d29f707cf1ae507a012dd5a810c81dee6
Chu Marie <marie.chu@tekos.net>
2023-05-01 15:27:04 +0700

    chore - edit base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://chumarie.github.io/my-media-server/' : '/',
+  base:'https://chumarie.github.io/my-media-server/',
   plugins: [react()],
   resolve: {
     alias: {
