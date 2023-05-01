8e4b30b84ceae26ffd38c5727b63144ebbb91421
Chu Marie <marie.chu@tekos.net>
2023-05-01 16:27:56 +0700

    fix - add console

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client'
 
 import App from '@presentation/App'
 
+console.log('before')
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+console.log('after')
