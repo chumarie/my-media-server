58c3a50516293c91b8cd592be5e3c23ba58db218
Chu Marie <marie.chu@tekos.net>
2023-05-01 15:52:49 +0700

    chore - edit url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-react-project/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-media-server/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
