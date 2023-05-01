8538a773d2a6c60c5f995ec8056a8aa9bf568410
Chu Marie <marie.chu@tekos.net>
2023-05-01 10:49:37 +0700

    fix - remove cors everywhere

--- src/infrastructure/config/axios.ts
+++ src/infrastructure/config/axios.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 const apiKey = import.meta.env.VITE_EMBY_SERVER_API_KEY;
-const corsProxy = "https://cors-anywhere.herokuapp.com/";
 
 const axiosInstance = axios.create({
-  baseURL: `${corsProxy}${import.meta.env.VITE_EMBY_SERVER_API_URL}`, // Utilisez la variable d'environnement définie dans votre fichier .env
+  baseURL: import.meta.env.VITE_EMBY_SERVER_API_URL, // Utilisez la variable d'environnement définie dans votre fichier .env
   timeout: 5000, // Temps d'attente en millisecondes avant d'abandonner la requête
   headers: {
     'Content-Type': 'application/json',
-    "x-requested-with": "XMLHttpRequest",
   },
 });
 
